ab20328badd350dbc4cb893e0c0932d1d555263b
MohammedAyman2018 <emidobibo20@gmail.com>
2020-05-10 14:32:24 +0200

    Fix customer table and get customer with _id

--- pages/customer/_id.vue
+++ pages/customer/_id.vue
@@ -1,39 +1,22 @@
 <template>
- <v-layout>
-    <h3>{{ this.articles}}</h3>
- </v-layout>
+	<v-layout>
+		<h3>{{ this.customer}}</h3>
+	</v-layout>
 </template>
 <script>
 export default {
-    middleware: 'auth',
-    layout : 'admin',
-     async fetch() {
-      try{
-      let response  = await this.$axios.get('/customers/');
-      let result = response.data.map(function(obj) {
-         return {
-           id: obj.userid,
-           customername: obj.customername,
-           orgnummer: obj.orgnummer,
-           vatnummer: obj.vatnummer,
-           postadress: obj.postadress,
-           stad: obj.stad,
-           kontaktperson: obj.kontaktperson,
-           country: obj.country,
-           overdueinterest: obj.overdueinterest +" %",
-         }
-       });
-       this.articles = result.filter(customer => customer.id == this.$route.params.id);
-     }
-     catch(e){
-      console.log(e)
-     }
-    },
-    data(){
-    return {
-        articles : [],
-    }
-    }
-}
+	middleware: "auth",
+	layout: "admin",
 
+	created() {
+		this.$axios
+			.$get(`/customers/${this.$route.params.id}`)
+			.then(res => (this.customer = res));
+	},
+	data() {
+		return {
+			customer: null
+		};
+	}
+};
 </script>
--- pages/customer/index.vue
+++ pages/customer/index.vue
@@ -1,101 +1,79 @@
 <template>
-  <v-layout>
-    <v-row>
-      <v-col cols="12" md="12">
-      <v-row class="pa-4">
-       <h1 class="title">Customers</h1>
-        <v-spacer></v-spacer>
-       <UserModal />
-          </v-row>
-      </v-col>
-       
-       <v-col cols="12" md="12">
-         <p class="overline"> All customers</p>
-          <v-card
-          tile
-          width="100%"
-          class="pa-8"
-          >
-          <v-row>
-           
-           <v-spacer></v-spacer>
-             <v-text-field
-               v-model="search"
-               append-icon="mdi-magnify"
-               label="Search"
-               single-line
-               hide-details
-               solo
-               dense
-               class="mb-4 pa-4"
-               color="#336882"
-                ></v-text-field>
-          </v-row>
-             <v-data-table
-              :headers="headers"
-              :items="articles"
-              :items-per-page="5"
-              class="elevation-1"
-              :search="search"
-              @click:row="handleClick"
-               ></v-data-table>
-          </v-card>
-       </v-col>
-    </v-row>
-  </v-layout>  
-</template>
-<script>
-import UserModal from '@/components/UserModal.vue';
-export default {
-   middleware: 'auth',
-    layout : 'admin',
-    components : {
-        UserModal,
-    },
-    async fetch() {
-      try{
-      let response  = await this.$axios.get('/customers/' );
-      let result = response.data.map(function(obj) {
-         return {
-           id: obj.userid,
-           customername: obj.customername,
-           orgnummer: obj.orgnummer,
-           vatnummer: obj.vatnummer,
-           postadress: obj.postadress,
-           stad: obj.stad,
-           kontaktperson: obj.kontaktperson,
-           country: obj.country,
-           overdueinterest: obj.overdueinterest +" %",
+	<v-layout>
+		<v-row>
+			<v-col cols="12" md="12">
+				<v-row class="pa-4">
+					<h1 class="title">Customers</h1>
+					<v-spacer></v-spacer>
+					<UserModal />
+				</v-row>
+			</v-col>
 
-         }
-       });
-       this.articles = result;
-     }
-     catch(e){
-      console.log(e)
-     }
-    },
-    methods : {
-      handleClick(a){
-      this.$router.push('/customer/'+a.id);
-    }
-    },
-    data () {
-      return {
-        search: '',
-        headers: [
-          {
-            text: 'Customer Id',
-            align: 'start',
-            sortable: false,
-            value: 'id',
-          },
-          { text: 'customername', value: 'customername' },
-          { text: 'vatnummer', value: 'vatnummer' },
-          { text: 'overdueinterest (%)', value: 'overdueinterest'},
-        ],
-        articles: [],
-      }
-    },
-}
+			<v-col cols="12" md="12">
+				<p class="overline">All customers</p>
+				<v-card tile width="100%" class="pa-8">
+					<v-row>
+						<v-spacer></v-spacer>
+						<v-text-field
+							v-model="search"
+							append-icon="mdi-magnify"
+							label="Search"
+							single-line
+							hide-details
+							solo
+							dense
+							class="mb-4 pa-4"
+							color="#336882"
+						></v-text-field>
+					</v-row>
+					<v-data-table
+						:headers="headers"
+						:items="customers"
+						:items-per-page="5"
+						class="elevation-1"
+						:search="search"
+						@click:row="handleClick"
+					></v-data-table>
+				</v-card>
+			</v-col>
+		</v-row>
+	</v-layout>
+</template>
+
+<script>
+import UserModal from "@/components/UserModal.vue";
+export default {
+	middleware: "auth",
+	layout: "admin",
+	components: {
+		UserModal
+	},
+	async created() {
+		await this.$axios.get("/customers/").then(res => {
+			this.customers = res.data;
+		});
+	},
+	methods: {
+		handleClick(a) {
+			this.$router.push("/customer/" + a._id);
+		}
+	},
+	data() {
+		return {
+			search: "",
+			headers: [
+				{
+					text: "Customer Id",
+					align: "start",
+					sortable: false,
+					value: "_id"
+				},
+				{ text: "customername", value: "customername" },
+				{ text: "vatnummer", value: "vatnummer" },
+				{ text: "overdueinterest (%)", value: "overdueinterest" }
+			],
+			customers: []
+		};
+	}
+};
 </script>
